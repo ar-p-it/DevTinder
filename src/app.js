af2a6e8fd33d7fdf350ef3e053f8600c828fdf74
Arpiit k <kesariarpit5@gmail.com>
2025-12-14 15:08:04 +0530

    routing (4)

--- src/app.js
+++ src/app.js
@@ -3,8 +3,15 @@ const express = require("express");
 const app = express();
 
 app.listen(3050, () => {
-  console.log("HIHIIHIHIHI");
+  console.log("Starting Server");
 });
-app.use("/test", (requess, respp) => {
-  respp.send("Samina wani");
+
+app.post("/test", (requess, respp) => {
+  respp.send(" wani");
+  console.log("Post done");
+});
+
+app.get("/test", (req, res) => {
+  console.log(req.query);
+  res.send("GG");
 });
